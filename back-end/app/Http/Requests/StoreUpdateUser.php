a4d68b88393e624b2ffa8c25c4cce8b7a265927c
kelvinhenri99 <kelvin5henri@gmail.com>
2023-05-03 10:43:42 -0300

    feat: create Routes and Controller Users

---
+++ back-end/app/Http/Requests/StoreUpdateUser.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdateUser extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'min:3',
+                'max:255'
+            ],
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                'unique:users'
+            ],
+            'password' => [
+                'required',
+                'min:6',
+                'max:255'
+            ]
+        ];
+    }
+}
